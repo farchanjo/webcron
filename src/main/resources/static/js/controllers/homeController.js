0e17de12cce1dd72ea22bbcabcd0c63921fe88a3
Fabricio Archanjo Fonseca <farchanjo@gmail.com>
2017-07-25 02:37:38 -0300

    Websocket working.

--- src/main/resources/static/js/controllers/homeController.js
+++ src/main/resources/static/js/controllers/homeController.js
@@ -1,10 +1,10 @@
 angular.module('webCronApp')
-    .controller('HomeCtrl', function ($scope, $route, $location, $log, $stomp) {
+    .controller('HomeCtrl', function ($scope, $route, $location, $log, $stomp, $cookies) {
         $stomp.setDebug(function (args) {
             $log.debug(args)
         });
 
-        $stomp.connect('/cronws', null)
+        $stomp.connect('/cronws', {"X-XSRF-TOKEN": $cookies.get("XSRF-TOKEN")})
         // frame = CONNECTED headers
             .then(function (frame) {
                 $stomp.subscribe('/topic/greetings', function (payload, headers, res) {
