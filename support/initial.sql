524e175ff99f2b4fc1ad082c5dac2eb18a479066
Fabricio Archanjo Fonseca <farchanjo@gmail.com>
2017-07-31 01:47:39 -0300

    SQL Updated.

--- support/initial.sql
+++ support/initial.sql
@@ -1,41 +1,70 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: vm.services    Database: webcrons
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
-/*!40103 SET TIME_ZONE = '+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
-/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `envs`
+--
+
+DROP TABLE IF EXISTS `envs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `envs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `key` varchar(255) NOT NULL,
+  `modified` datetime NOT NULL,
+  `value` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `envs`
+--
+
+LOCK TABLES `envs` WRITE;
+/*!40000 ALTER TABLE `envs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `envs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `jobs`
 --
 
 DROP TABLE IF EXISTS `jobs`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `jobs` (
-  `id`         BIGINT(20)   NOT NULL AUTO_INCREMENT,
-  `async`      INT(11)      NOT NULL,
-  `command`    TEXT         NOT NULL,
-  `created`    DATETIME     NOT NULL,
-  `cron`       VARCHAR(255)          DEFAULT NULL,
-  `directory`  VARCHAR(255)          DEFAULT NULL,
-  `envs`       LONGBLOB,
-  `fixed_rate` INT(11)               DEFAULT NULL,
-  `modified`   DATETIME     NOT NULL,
-  `name`       VARCHAR(255) NOT NULL,
-  `status`     INT(11)      NOT NULL,
-  `unit`       INT(11)               DEFAULT NULL,
-  `user_id`    BIGINT(20)            DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `async` int(11) NOT NULL,
+  `command` text NOT NULL,
+  `created` datetime NOT NULL,
+  `cron` varchar(255) DEFAULT NULL,
+  `directory` varchar(255) DEFAULT NULL,
+  `fixed_rate` int(11) DEFAULT NULL,
+  `modified` datetime NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `unit` int(11) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKra3g6pshf0p0hv5aisuh3weg8` (`user_id`),
   CONSTRAINT `FKra3g6pshf0p0hv5aisuh3weg8` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-)
-  ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,10 +72,34 @@ CREATE TABLE `jobs` (
 --
 
 LOCK TABLES `jobs` WRITE;
-/*!40000 ALTER TABLE `jobs`
-  DISABLE KEYS */;
-/*!40000 ALTER TABLE `jobs`
-  ENABLE KEYS */;
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jobs_environments`
+--
+
+DROP TABLE IF EXISTS `jobs_environments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jobs_environments` (
+  `jobs_id` bigint(20) NOT NULL,
+  `environments_id` bigint(20) NOT NULL,
+  UNIQUE KEY `UK_8cgscl78u0vwj2xr5nel04iyo` (`environments_id`),
+  KEY `FKmo9j12lm7265as44turikpt3p` (`jobs_id`),
+  CONSTRAINT `FKm8g6es4qxc7h3jyejett2yofc` FOREIGN KEY (`environments_id`) REFERENCES `envs` (`id`),
+  CONSTRAINT `FKmo9j12lm7265as44turikpt3p` FOREIGN KEY (`jobs_id`) REFERENCES `jobs` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jobs_environments`
+--
+
+LOCK TABLES `jobs_environments` WRITE;
+/*!40000 ALTER TABLE `jobs_environments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jobs_environments` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -54,23 +107,20 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id`       BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `created`  DATETIME            DEFAULT NULL,
-  `email`    VARCHAR(255)        DEFAULT NULL,
-  `modified` DATETIME            DEFAULT NULL,
-  `name`     VARCHAR(255)        DEFAULT NULL,
-  `password` VARCHAR(255)        DEFAULT NULL,
-  `role`     INT(11)             DEFAULT NULL,
-  `status`   INT(11)             DEFAULT NULL,
-  `username` VARCHAR(255)        DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-)
-  ENGINE = InnoDB
-  AUTO_INCREMENT = 2
-  DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,21 +128,18 @@ CREATE TABLE `users` (
 --
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users`
-  DISABLE KEYS */;
-INSERT INTO `users` VALUES (1, '2017-07-30 14:21:03', 'admin@admin.com', '2017-07-30 14:21:03', 'Admin',
-                            '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 1, 0, 'admin');
-/*!40000 ALTER TABLE `users`
-  ENABLE KEYS */;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'2017-07-31 01:46:03','admin@admin.com','2017-07-31 01:46:03','Admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',1,0,'admin');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-30 14:21:32
+-- Dump completed on 2017-07-31  1:46:32
